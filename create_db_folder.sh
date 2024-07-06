f9852b3f71996b0e506d20c7eea6e712420501b4
MichaelAwadEissa <michaelawadeissa@gmail.com>
2024-07-06 18:47:55 +0300

    creat_db_folder

---
+++ create_db_folder.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Function to create Data_Base folder if it doesn't exist
+create_database_folder() {
+    folder_name="Data_Base"
+    
+    if [ ! -d "$folder_name" ]; then
+        mkdir "$folder_name"
+        echo "Folder '$folder_name' created."
+    else
+        echo "Folder '$folder_name' already exists."
+    fi
+}
+
+# Call the function
+create_database_folder
